5b7d0e3d2edf6e2068af910b47a4299dde547c51
Yair5675 <yairziv5675@gmail.com>
2023-08-07 19:54:25 +0300

    Removed the ability to decide the salt's size (complicates decryption later) and set it to always be 32 bytes as a private and constant class attribute

--- cryptotools/encryptor.py
+++ cryptotools/encryptor.py
@@ -72,24 +72,18 @@ class Encryptor:
         ]
 
     # Constants in the class:
-    MAX_SALT_SIZE = 32  # Maximum amount of bytes that will be dedicated to the salt (user can choose less)
+    __SALT_SIZE = 32  # The amount of bytes that will be dedicated to the salt
 
     __KEY_LENGTH = 32  # The amount of bytes the key will be made of (multiply by 8 to get the amount of bits)
     __KEY_ITERATIONS = 100000  # Number of iteration to create the encryption key (higher is more secure but slower)
 
-    def __init__(self, password: str, salt_size: int):
+    def __init__(self, password: str):
         # Ensuring type safety for the password:
         if type(password) != str:
             raise TypeError(f'Expected a password of type str, got {type(password)} instead')
-        # Ensuring the salt size is a valid integer between 1 and the maximum amount of salt bytes:
-        if type(salt_size) != int:
-            raise TypeError(f'Expected an unsigned integer as salt size, got {type(salt_size)} instead')
-        elif salt_size <= 0 or salt_size > Encryptor.MAX_SALT_SIZE:
-            raise ValueError(f'Invalid value for salt size: {salt_size} (should be between 1 and {Encryptor.MAX_SALT_SIZE} '
-                             'inclusively)')
 
         # Generate random salt bytes for the encryption:
-        self.__salt = os.urandom(salt_size)
+        self.__salt = os.urandom(Encryptor.__SALT_SIZE)
         # Derive key:
         self.__key = Encryptor.derive_key(password, self.__salt)
         # Initializing the saved data and the 'is_encrypted' attribute:
