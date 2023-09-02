34f0ad32d282fc3a48751d8172e64c0aef3116ce
Yair5675 <yairziv5675@gmail.com>
2023-09-02 23:38:01 +0300

    Added the private attribute "password" to the Decryptor class with a setter method, and added a constructor that takes a password as a parameter

--- cryptotools/decryptor.py
+++ cryptotools/decryptor.py
@@ -1,5 +1,31 @@
 class Decryptor:
     """
-    A class that is used to decrypt encrypted data using a password-based key.
+    A class that is used to decrypt encrypted data using a password-based key. The class is meant to decrypt data which was
+    encrypted using the Encryptor class, therefor it is not for general purpose decryption.
     """
-    pass
+    __slots__ = [
+        '__password'  # The password used during the encryption. It is needed to reverse the process
+    ]
+
+    def __init__(self, password: str):
+        """
+        The constructor of the Decryptor class.
+        :param password: A piece of string that was used in the encryption process to derive an encryption key. It will be
+                         used in the decryption process as well.
+        :type password: str
+        :rtype: Decryptor
+        """
+        # Setting the password:
+        self.set_password(password)
+
+    def set_password(self, password: str):
+        """
+        A setter method for the password inside the Decryptor class.
+        :param password: A piece of string that was used in the encryption process to derive an encryption key. It will be
+                         used in the decryption process as well.
+        :type password: str
+        """
+        # Ensuring type safety for the password:
+        if type(password) != str:
+            raise TypeError(f'Expected a password of type str, got {type(password)} instead')
+        self.__password = password
