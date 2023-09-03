0560c55a7e9d485d60779825847fe8ac48589564
Yair5675 <yairziv5675@gmail.com>
2023-09-03 19:39:22 +0300

    Created a custom exceptions for cases where the Decryptor tries to perform an action that requires the data to be not decrypted, but it was

--- cryptotools/decryptor.py
+++ cryptotools/decryptor.py
@@ -15,6 +15,15 @@ class DecryptorException(Exception):
         super().__init__(message)
 
 
+class DataAlreadyDecryptedException(Exception):
+    """
+    An exception for cases where the Decryptor instance is trying to perform an action that requires the data to not be
+    decrypted, while it had already been decrypted.
+    """
+    def __init__(self, message):
+        super().__init__(message)
+
+
 ###################
 # The class itself:
 ###################
